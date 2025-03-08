e1a33e9eee1cb7678b58b8ee7d1b8834e6525da2
Sanup Parajuli <sanupparajuli1@gmail.com>
2025-03-08 23:44:31 +0545

    add getuserprofile api

--- src/api/api.js
+++ src/api/api.js
@@ -131,3 +131,12 @@ export const deleteTweet = async (tweetId) => {
     throw error.response ? error.response.data : error.message;
   }
 };
+export const getUserProfile = async () => {
+  try {
+    const response = await API.get("/users/profile");
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching profile:", error);
+    throw error;
+  }
+};
